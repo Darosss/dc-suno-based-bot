107bb323b8a8ddbe0f32b38238656d924c186d7a
Darosss <daroing12@gmail.com>
2024-05-31 23:04:38 +0200

    feat(StartPlay): add radio max + possibility to change count

--- src/commands/start-play.ts
+++ src/commands/start-play.ts
@@ -1,19 +1,41 @@
 import PlayerQueue from "@/src/player-queue";
 import { getMp3FromMusicFolder } from "@/utils/mp3.utils";
 import { Message } from "discord.js";
+import { CommandsType } from "./commands-list";
+import { DEFAULT_MAX_RADIO_SONGS } from "@/src/globals";
+import { removeCommandNameFromMessage } from "../utils/dc.utils";
 
-export const startPlayCommand = (message: Message) => {
+export const startPlayCommand = (
+  message: Message,
+  commandData: CommandsType
+) => {
   if (message.member?.id !== process.env.OWNER_ID)
     return message.reply("Only owner can do this (for now) ");
-  const files = getMp3FromMusicFolder().sort(() => 0.5 - Math.random());
 
-  files.forEach((mp3File) => {
+  const files = getMp3FromMusicFolder().sort(() => 0.5 - Math.random());
+  let maxSongs =
+    files.length > DEFAULT_MAX_RADIO_SONGS
+      ? DEFAULT_MAX_RADIO_SONGS
+      : files.length;
+
+  const numberOfSongs = Number(
+    removeCommandNameFromMessage(message.content, commandData)
+  );
+
+  if (
+    numberOfSongs !== 0 &&
+    !isNaN(numberOfSongs) &&
+    numberOfSongs < DEFAULT_MAX_RADIO_SONGS
+  ) {
+    maxSongs = files.length > numberOfSongs ? numberOfSongs : files.length;
+  }
+
+  for (let songIndex = 0; songIndex < maxSongs; songIndex++) {
     PlayerQueue.enqueue({
-      name: mp3File,
+      name: files[songIndex],
       requester: message.author.id
     });
-  });
-  console.log(files, "aha");
+  }
 
   if (files.length === 0) {
     return message.reply("No music files found in the /music folder.");

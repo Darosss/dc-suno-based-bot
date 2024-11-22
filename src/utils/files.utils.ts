f54af6975551d69998e1962d4ae6df3be1ed2938
darosss <darosss12@proton.me>
2024-11-22 21:42:27 +0100

    feat(files.utils): add deleteFile function

--- src/utils/files.utils.ts
+++ src/utils/files.utils.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { TODO } from "../types";
 
 export const isFileAccesilbe = async (
   absolutePath: string
@@ -8,5 +9,21 @@ export const isFileAccesilbe = async (
     return true;
   } catch (err) {
     return false;
+  }
+};
+
+export const deleteFile = async (filePath: string) => {
+  try {
+    await fs.promises.unlink(filePath);
+    console.log(`File at ${filePath} deleted successfully.`);
+  } catch (error) {
+    if ("code" in (error as TODO) && (error as TODO).code === "ENOENT") {
+      console.log(`File at ${filePath} does not exist, no action taken.`);
+    } else {
+      console.error(
+        "Error occured while trying to remove exceeding data from mp3 folder",
+        error
+      );
+    }
   }
 };

2b449f1d0ebeafc4d9fd6c89ffa0a015e5c3b92c
darosss <darosss12@proton.me>
2025-03-04 20:51:04 +0100

    feat(radio): improve radio with only-downloaded or not - add only-downloaded optional option (only for backslash commands for now)

--- src/commands/start-play.ts
+++ src/commands/start-play.ts
@@ -1,5 +1,9 @@
 import PlayerQueue from "@/src/player-queue";
-import { getMp3FromMusicFolder } from "@/utils/mp3.utils";
+import {
+  getAllPossibleAudios,
+  getMp3FromMusicFolder,
+  getStoredSongDataFromFileName
+} from "@/utils/mp3.utils";
 import { GuildMember, Message, SlashCommandBuilder } from "discord.js";
 import { COMMANDS } from "./commands-list";
 import {
@@ -9,11 +13,13 @@ import {
 import {
   BaseExecuteOptions,
   MessageCommandType,
-  MessageInteractionTypes
+  MessageInteractionTypes,
+  StoredSongData
 } from "@/src/types";
 import ConfigsHandler from "@/src/utils/configs.utils";
 
 const SLASH_COMMAND_OPTION_SONG_COUNT = "songs-count";
+const SLASH_COMMAND_OPTION_SONG_TYPE = "only-downloaded";
 const COMMAND_DATA = COMMANDS.radio;
 
 const startPlayCommand = async (message: Message) => {
@@ -30,24 +36,43 @@ const slashStartPlayCommand = async (message: MessageInteractionTypes) => {
   const songsCount = message.options.get(
     SLASH_COMMAND_OPTION_SONG_COUNT
   )?.value;
+  const onlyDownloaded = !!message.options.get(SLASH_COMMAND_OPTION_SONG_TYPE)
+    ?.value;
 
   await message.reply("Trying to set radio on");
   const returnMessage = await startPlayCommandLogic(
     message,
-    (songsCount as number) || ConfigsHandler.getConfigs().maxRadioSongs
+    (songsCount as number) || ConfigsHandler.getConfigs().maxRadioSongs,
+    onlyDownloaded
   );
   return await message.editReply(returnMessage);
 };
 
+const getRadioSongsHelper = async (onlyDownloaded?: boolean) => {
+  if (onlyDownloaded) {
+    return (await getMp3FromMusicFolder()).sort(() => 0.5 - Math.random());
+  } else {
+    const allPossibleAudios = (await getAllPossibleAudios())
+      .split("\n")
+      .sort(() => 0.5 - Math.random())
+      .filter(Boolean);
+
+    return allPossibleAudios.map((fileName) =>
+      getStoredSongDataFromFileName(fileName)
+    );
+  }
+};
+
 const startPlayCommandLogic = async (
   message: MessageCommandType,
-  numberOfSongs: number
+  numberOfSongs: number,
+  onlyDownloaded?: boolean
 ): Promise<string> => {
   const { success, message: messageInfo } =
     handleBotConnectionToVoiceChannel(message);
   if (!success) return messageInfo || "Something went wrong";
 
-  const files = (await getMp3FromMusicFolder()).sort(() => 0.5 - Math.random());
+  const files = await getRadioSongsHelper(onlyDownloaded);
   const maxRadioSongs = ConfigsHandler.getConfigs().maxRadioSongs;
   const maxSongs =
     numberOfSongs > maxRadioSongs
@@ -81,6 +106,11 @@ const data = new SlashCommandBuilder()
       .setDescription("Set count of radio songs")
       .setMaxValue(ConfigsHandler.getConfigs().maxRadioSongs)
       .setMinValue(2)
+  )
+  .addBooleanOption((option) =>
+    option
+      .setName(SLASH_COMMAND_OPTION_SONG_TYPE)
+      .setDescription("Only already downloaded?")
   )
   .setDescription(COMMAND_DATA.description)
   .setDefaultMemberPermissions("0");
